9a347433f0e6ce325845f7efcf06eeb6dbd966c5
unknown <Bernie.xiong@086010RD07W.bj.i-click.cn>
2015-01-07 16:10:05 +0800

    add list function

--- cmd2/cmd/tar.py
+++ cmd2/cmd/tar.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 '''
-	利用tarfile实现Linux上的简易版tar命令
+	利用tarfile实现Linux上的简易版tar命令,主要实现 压缩，解压，列举三个功能
 	tar -czvf tarname file1 file2
 	tar -xzvf tarname
+	tar -tzvf tarname
 	tar -cjvf zipfile f1 file2
 	tar -xjvf zipfile
 	等。参数可以随意组合
@@ -12,11 +13,11 @@
 import getopt,os.path,os
 import sys,tarfile,bz2
 
-rw = fmt = dest_path = tar_name =args =''
-show=c= False
+rw = fmt = dest_path = tar_name =args = ''
+show=c=ls= False
 def parse_args():
 	'''接收命令行参数'''
-	global c,show,dest_path,tar_name,rw,fmt,args
+	global c,show,dest_path,tar_name,rw,fmt,args,ls
 	try:
 	    opts, args = getopt.getopt(sys.argv[1:],"cxzvjtf:",["create","extract","gz","view","bzip2","list","file="])
 	except getopt.GetoptError, err:
@@ -35,6 +36,8 @@ def parse_args():
 			fmt = 'gz'
 		elif o in ('-j','--bzip2'):
 			fmt = 'bz2'	
+		elif o in ('-t','--list'):
+			ls = True
 		elif o in ('-f','--file'):
 			if '/' or '\\' in a:
 				dest_path,tar_name = os.path.split(a)
@@ -47,20 +50,34 @@ def create_tarfile(src_path):
 	for root,dirs,files in os.walk(src_path):
 		for f in files:
 			if f in args:
+				fullpath = os.path.join(root,f)
 				if show:
-					fullpath = os.path.join(root,f)
 					print(fullpath)
 				tar.add(f)
+		for d in dirs:
+			if d in args:
+				fullpath = os.path.join(root,d)
+				if show:
+					print(fullpath)
+				tar.add(d)
 	tar.close()
 
 def decompression():
 	'''解压缩'''
-	print(os.path.join(os.getcwd(),os.path.basename(tar_name).split('.')[0]))
-	
 	etar = tarfile.open(os.path.join(os.getcwd(),dest_path,tar_name),rw+':'+fmt)
 	for tarinfo in etar:
-		etar.extract(tarinfo, os.path.basename(tar_name).split('.')[0])	
+		import time
+		timeArray = time.localtime(tarinfo.mtime)
+		d = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+		if show:
+			print('{0}k  {1}  {2}'.format(str(tarinfo.size).rjust(5),d,tarinfo.name))
+		if not ls:
+			etar.extract(tarinfo, os.path.basename(tar_name).split('.')[0])	
+	
+	if not ls:
+		print('\ndecompression to:%s' %os.path.join(os.getcwd(),os.path.basename(tar_name).split('.')[0]))
 	etar.close()
+
 
 def main():
 	parse_args()
